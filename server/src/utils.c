9f0e88f95f22c8670c1475c53b297cfcc5ce17e0
Tomas Rodriguez Laise <113038407+TRLaise@users.noreply.github.com>
2025-04-29 00:31:27 -0300

    Update utils.c

--- server/src/utils.c
+++ server/src/utils.c
@@ -1,62 +1,84 @@
-#include"utils.h"
+#include "utils.h"
 
-t_log* logger;
+t_log *logger;
 
-int iniciar_servidor(void)
+int iniciar_servidor(char *puerto, t_log *un_log, char *msj_server)
 {
-	// Quitar esta línea cuando hayamos terminado de implementar la funcion
-	assert(!"no implementado!");
-
 	int socket_servidor;
 
-	struct addrinfo hints, *servinfo, *p;
+	struct addrinfo hints, *servinfo;
 
 	memset(&hints, 0, sizeof(hints));
 	hints.ai_family = AF_INET;
-	hints.ai_socktype = SOCK_STREAM;
-	hints.ai_flags = AI_PASSIVE;
 
-	getaddrinfo(NULL, PUERTO, &hints, &servinfo);
+	hints.ai_flags = AI_PASSIVE;	hints.ai_socktype = SOCK_STREAM;
 
-	// Creamos el socket de escucha del servidor
+	getaddrinfo(NULL, puerto, &hints, &servinfo);
 
-	// Asociamos el socket a un puerto
+ 	// Creamos el socket de escucha del servidor
+ 	socket_servidor = socket(servinfo->ai_family,
+                          servinfo->ai_socktype,
+                          servinfo->ai_protocol);
+	if (socket_servidor == -1) {
+        perror("Error en crear socket de escucha");
+        freeaddrinfo(servinfo);
+        return -1;
+    }
 
-	// Escuchamos las conexiones entrantes
+    // Asociar el socket a un puerto
+    if (bind(socket_servidor, servinfo->ai_addr, servinfo->ai_addrlen) == -1) {
+        perror("Error en bind");
+        close(socket_servidor);
+        freeaddrinfo(servinfo);
+        return -1;
+    }
+
+    // Escuchar las conexiones entrantes
+    if (listen(socket_servidor, SOMAXCONN) == -1) {
+        perror("Error en listen");
+        close(socket_servidor);
+        freeaddrinfo(servinfo);
+        return -1;
+    }
 
 	freeaddrinfo(servinfo);
-	log_trace(logger, "Listo para escuchar a mi cliente");
+	log_info(un_log, "SERVER: %s", msj_server);
 
 	return socket_servidor;
 }
 
-int esperar_cliente(int socket_servidor)
+int esperar_cliente(int socket_servidor, t_log *un_log, char *mensaje)
 {
-	// Quitar esta línea cuando hayamos terminado de implementar la funcion
-	assert(!"no implementado!");
-
 	// Aceptamos un nuevo cliente
-	int socket_cliente;
-	log_info(logger, "Se conecto un cliente!");
+	int socket_cliente = accept(socket_servidor, NULL, NULL);
 
+	if (socket_cliente == -1)
+	{
+		perror("Error al aceptar la conexión");
+		return -1;
+	}
+	log_info(un_log, "%s fd:%d", mensaje, socket_cliente);
 	return socket_cliente;
 }
 
 int recibir_operacion(int socket_cliente)
 {
 	int cod_op;
-	if(recv(socket_cliente, &cod_op, sizeof(int), MSG_WAITALL) > 0)
+    ssize_t bytes = recv(socket_cliente, &cod_op, sizeof(int), MSG_WAITALL);
+	if (bytes > 0)
 		return cod_op;
-	else
-	{
-		close(socket_cliente);
-		return -1;
-	}
+    else if (bytes == 0)
+    {
+        perror("El cliente cerro la conexion");
+    }
+	close(socket_cliente);
+	return -1;
+
 }
 
-void* recibir_buffer(int* size, int socket_cliente)
+void *recibir_buffer(int *size, int socket_cliente)
 {
-	void * buffer;
+	void *buffer;
 
 	recv(socket_cliente, size, sizeof(int), MSG_WAITALL);
 	buffer = malloc(*size);
@@ -68,27 +90,27 @@ void* recibir_buffer(int* size, int socket_cliente)
 void recibir_mensaje(int socket_cliente)
 {
 	int size;
-	char* buffer = recibir_buffer(&size, socket_cliente);
+	char *buffer = recibir_buffer(&size, socket_cliente);
 	log_info(logger, "Me llego el mensaje %s", buffer);
 	free(buffer);
 }
 
-t_list* recibir_paquete(int socket_cliente)
+t_list *recibir_paquete(int socket_cliente)
 {
 	int size;
 	int desplazamiento = 0;
-	void * buffer;
-	t_list* valores = list_create();
+	void *buffer;
+	t_list *valores = list_create();
 	int tamanio;
 
 	buffer = recibir_buffer(&size, socket_cliente);
-	while(desplazamiento < size)
+	while (desplazamiento < size)
 	{
 		memcpy(&tamanio, buffer + desplazamiento, sizeof(int));
-		desplazamiento+=sizeof(int);
-		char* valor = malloc(tamanio);
-		memcpy(valor, buffer+desplazamiento, tamanio);
-		desplazamiento+=tamanio;
+		desplazamiento += sizeof(int);
+		char *valor = malloc(tamanio);
+		memcpy(valor, buffer + desplazamiento, tamanio);
+		desplazamiento += tamanio;
 		list_add(valores, valor);
 	}
 	free(buffer);
